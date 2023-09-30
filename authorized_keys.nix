258375a44ad1796b260f824d6b050d804574ca2b
Gilles Coremans <gilles.coremans@gmail.com>
2023-09-30 11:08:08 +0200

    Add vigilant pubkey

--- authorized_keys.nix
+++ authorized_keys.nix
@@ -1,4 +1,4 @@
 [
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6T5mOB1QA2mEKx9o85KBmXmkpeOVdL12DBqVU5v1JF gilles@valentine"
-  # TODO: add vigilant key
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQJ060RHaZHlJTP7TepkbmQ1JyJKxDGCa6bN7xPux1A gilles@vigilant"
 ]
