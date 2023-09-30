40436cea6f1606a9aa9334e0b2e8714cebdb4991
Gilles Coremans <gilles.coremans@gmail.com>
2023-09-30 12:14:52 +0200

    Integrate nyave hardware config

--- nixos/nyave.nix
+++ nixos/nyave.nix
@@ -10,5 +10,29 @@
   };
 
   services.openssh.permitRootLogin = "yes";
-  nixpkgs.hostPlatform = "aarch64-linux";
+
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "virtio_scsi" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/b012e766-7cab-4a2c-9031-fc24946e05fa";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/7D28-308C";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
