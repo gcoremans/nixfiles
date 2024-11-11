f11ed7b054bc55107906e269f94d16b07f912907
Gilles Coremans <gilles.coremans@gmail.com>
2024-11-11 19:40:23 +0100

    Add aesma hardware options

--- hosts/aesma.nix
+++ hosts/aesma.nix
@@ -3,10 +3,6 @@
   networking.hostName = "aesma";
   system.stateVersion = "24.05";
 
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
-
   users.users = {
     root = {
       openssh.authorizedKeys.keys = authorizedKeys;
@@ -19,6 +15,26 @@
   services.openssh.settings.PermitRootLogin = "prohibit-password";
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_scsi" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/e80f9ae3-a39e-474d-b5f1-050bf15d917f";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/9152-1F73";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 
   networking.useDHCP = lib.mkDefault true;
 }
