c552645e3b8a72d6d2df1be909452a6f38606be7
Gilles Coremans <gilles.coremans@gmail.com>
2024-11-11 19:56:02 +0100

    Add ipv6 config to aesma

--- hosts/aesma.nix
+++ hosts/aesma.nix
@@ -36,5 +36,17 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  networking.useDHCP = lib.mkDefault true;
+  networking = {
+    useDHCP = true;
+    interfaces.enp1s0 = {
+      ipv6.addresses = [{
+        address = "2a01:4f8:c013:833f::1";
+        prefixLength = 64;
+      }];
+    };
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp1s0";
+    };
+  };
 }
